9c295f0dd1f6800338400a0240cf695fc59b6476
Gabriel Mercado Mendez <gabomm99@ad.unc.edu>
2022-03-04 11:54:11 -0500

    All endpoints seem to work

--- server.js
+++ server.js
@@ -61,34 +61,43 @@ const server = app.listen(port, () =>{
 
 
 app.get('/app', (req, res) => {
-    res.statusCode = 200
-    res.statusMessage = 'OK'
-    res.setHeader(res.statusCode, {'Content-Type' : 'text/plain'})
-    res.end(res.statusCode+ ' ' + res.statusMessage)
+    res.type('text/plain')
+    res.status(200).end(`200 OK`)
 }
 )
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
-    res.type("text/json")
+    res.type('application/json')
     res.status(200).json({'flip': flip})
 })
 
 app.get('/app/flips/:number', (req,res) => {
     var manyFlip = coinFlips(req.params.number)
     var sumFlip = countFlips(manyFlip)
-    res.type("text/json")
-    res.status(200).json({'raw': manyFlip})
+    res.type('application/json')
+    res.status(200).json({'raw': manyFlip, 'summary': sumFlip})
 })
 
 
-app.get('/app/flip/call/:coinSide', (req, res) => {
-  var flipResult = flipACoin()
-  res.status(200).send({flipResult})
-  res.type("text/plain")
+app.get('/app/flip/call/heads', (req, res) => {
+  var flipResult = flipACoin("heads")
+  var call = flipResult.call
+  var flip = flipResult.flip
+  var result = flipResult.result
+  res.type('application/json')
+  res.status(200).json({'call': call, 'flip': flip, 'result': result})
 })
 
+app.get('/app/flip/call/tails', (req, res) => {
+  var flipResult = flipACoin("tails")
+  var call = flipResult.call
+  var flip = flipResult.flip
+  var result = flipResult.result
+  res.type('application/json')
+  res.status(200).json({'call': call, 'flip': flip, 'result': result})
+})
 
 app.use(function(req, res){
     res.status(404).send("Endpoint does not exist")
-    res.type("text/plain")
+    res.type('text/plain')
 })
