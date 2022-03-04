6ba9d98120a8f319848018aa08385eacdef863bb
Gabriel Mercado Mendez <gabomm99@ad.unc.edu>
2022-03-04 09:36:00 -0500

    All endpoints created, only flip works

--- server.js
+++ server.js
@@ -1,36 +1,93 @@
+function coinFlip() {
+  let result
+  let flip = Math.random()
+  if ( flip < 0.5) {
+    result = "heads"
+  } else {
+    result = "tails"
+  }
+  return result
+}
+
+function coinFlips(flips) {
+  let multiFlip = []
+  let i = 0
+  while (i < flips){
+    multiFlip.push(coinFlip())
+    i++
+  }
+  return multiFlip
+}
+
+function countFlips(array) {
+  let counter = {heads: 0, tails: 0} 
+  for (let flip in array){
+    if (array[flip] == "tails"){
+      counter.tails++
+    }
+    else{
+      counter.heads++
+    }
+  }
+  return counter
+}
+
+function flipACoin(call) {
+  let outcome = "lose"
+  let flipOut = coinFlip()
+  if (flipOut == call){
+    outcome = "win"
+  }
+  let scenario = {call: call, flip: flipOut, result: outcome }
+  return scenario
+}
+
+
+
+
+
 const express = require('express')
 const app = express()
 
-var port = 5555
+
+
+const args = require("minimist")(process.argv.slice(2))
+args["port"]
+var port = args.port || process.env.PORT || 5555
 
 const server = app.listen(port, () =>{
-    console.log('App is running on port %PORT%'.replace('%PORT', port))
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
 })
 
-function coinFlip() {
-    let result
-    let flip = Math.random()
-    if ( flip < 0.5) {
-      result = "heads"
-    } else {
-      result = "tails"
-    }
-    return result
-  }
 
 app.get('/app', (req, res) => {
-    res.status(200).end('API is working')
-    res.type('text/plain')
+    res.statusCode = 200
+    res.statusMessage = 'OK'
+    res.setHeader(res.statusCode, {'Content-Type' : 'text/plain'})
+    res.end(res.statusCode+ ' ' + res.statusMessage)
 }
 )
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
+    res.type("text/json")
     res.status(200).json({'flip': flip})
 })
 
-app.get('/app/echo/:number', (req,res) => {
-    res.status(200).json({ 'message': req.params.number})
+app.get('/app/flips/:number', (req,res) => {
+    var manyFlip = coinFlips(req.params.number)
+    var sumFlip = countFlips(manyFlip)
+    res.type("text/json")
+    res.status(200).json({'raw': manyFlip})
 })
+
+
+app.get('/app/flip/call/:coinSide', (req, res) => {
+  var flipResult = flipACoin()
+  res.status(200).send({flipResult})
+  res.type("text/plain")
+})
+
+
 app.use(function(req, res){
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
